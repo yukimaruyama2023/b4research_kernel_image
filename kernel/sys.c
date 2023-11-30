ad0120f72b91cd3969c6862e255b2bc35b4c66cf
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2023-11-30 06:23:07 +0000

    able to get cpu_metrics

--- kernel/sys.c
+++ kernel/sys.c
@@ -944,7 +944,7 @@ SYSCALL_DEFINE1(setfsgid, gid_t, gid)
  */
 SYSCALL_DEFINE0(getpid)
 {
-    printk("10/26 watashi no kernel!\n");
+    printk("11/6 watashi no kernel!\n");
 	return task_tgid_vnr(current);
 }
 
